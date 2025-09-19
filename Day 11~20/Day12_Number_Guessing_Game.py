66c99b66778845d660a1fe7a3c2a3cd624c96878
jintol <jintol1891@naver.com>
2025-09-19 21:18:44 +0900

    250919

--- Day 11~20/Day12_Number_Guessing_Game.py
+++ Day 11~20/Day12_Number_Guessing_Game.py
@@ -75,10 +75,95 @@
 # Day 12 Project
 # Making the Number Guessing Game project
 
+# 내가 찾은 답!
 
+# import random
 
+# randnum = random.randint(1, 100)
 
+# print("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.")
+# level = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
 
+# game_over = False
 
+# life = 0
 
+# if level == "easy":
+#     life = 10
+# elif level == "hard":
+#     life = 5
+# else:
+#     print("Refresh the page to run again.")
+#     game_over = True
+
+# def lifes():
+#     print(f"You have {life} attempts remaining to guess the number.")
+
+# def decrease_enemies(lifess):
+#     return lifess - 1
+
+# while not game_over:
+#     lifes()
+#     life = decrease_enemies(life)
+#     guessnum = int(input("Make a guess:"))
+    
+#     if guessnum == randnum:
+#         print(f"You got it! The answer was {randnum}.")
+#         print("Refresh the page to run again.")
+#         game_over = True
+#     elif life == 0:
+#         print("You've run out of guesses. Refresh the page to run again.")
+#         game_over = True
+#     else:
+#         if guessnum > randnum:
+#             print("Too high")
+#         elif guessnum < randnum:
+#             print("Too low")
+
+# 정답
+
+from random import randint
+
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+turns = 0
+
+def check_answer(user_guess, actual_answer, turns): # 여기에 turns 넣어서 전역변수 사용
+    '''Checks answer against guess, returns the number of turns remaining.'''
+    if user_guess > actual_answer:
+        print("Too high.")
+        return turns - 1
+    elif user_guess < actual_answer:
+        print("Too low.")
+        return turns - 1
+    else:
+        print(f"You got it! The answer was {actual_answer}")
+
+def set_difficulty():
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+    if level == "easy":
+        global turns
+        return EASY_LEVEL_TURNS
+    elif level == "hard":
+        global turns
+        return HARD_LEVEL_TURNS
+
+def game():
+    print("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.")
+    answer = randint(1, 100)
+
+    turns = set_difficulty()
+    
+    guess = 0
+    while guess != answer:
+        print(f"You have {turns} attempts remaining to guess the number.")
+        guess = int(input("Make a guess:"))
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print("You've run out of guesses. Refresh the page to run again.")
+            return 
+        elif guess != answer:
+            print("Guess again.")
+game()
 
---
+++ Day 11~20/Day13_Debugging.py
@@ -0,0 +1,10 @@
+# 250918~19 Day 9 ~ 10 of Python
+
+
+
+
+
+
+
+# Day 13 Project
+# Debugging
